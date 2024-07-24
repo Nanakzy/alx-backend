73ea519e39f732dd1d3b4fa3a635fbb9bc273e39
Nanakzy <nanakzy@gmail.com>
2024-07-24 20:54:00 +0000

    4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,31 +1,48 @@
 #!/usr/bin/env python3
-""" MRUCache module """
+""" MRU Caching module """
 
-from base_caching import BaseCaching
 from collections import OrderedDict
+from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRUCache class """
+    """ MRUCache defines a Most Recently Used (MRU) caching system """
 
     def __init__(self):
-        """ Initialize """
+        """ Initialize MRUCache """
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.cache_order = OrderedDict()
 
     def put(self, key, item):
         """ Add an item in the cache """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.cache_data.move_to_end(key)
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                discarded_key, _ = self.cache_data.popitem(last=True)
-                print("DISCARD: {}".format(discarded_key))
+            self.cache_order.move_to_end(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                self._evict()
+            self.cache_data[key] = item
+            self.cache_order[key] = item
 
     def get(self, key):
         """ Get an item by key """
-        if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key)
-            return self.cache_data[key]
-        return None
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.cache_order.move_to_end(key)
+        return self.cache_data[key]
+
+    def _evict(self):
+        """ Evict the most recently used item """
+        key, _ = self.cache_order.popitem(last=True)
+        del self.cache_data[key]
+        print(f"DISCARD: {key}")
+
+    def print_cache(self):
+        """ Print the cache """
+        print("Current cache:")
+        for key in self.cache_data:
+            print(f"{key}: {self.cache_data[key]}")
