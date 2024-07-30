0eb6b77dbbe27e65649186df5da8a39eeea81d17
Nanakzy <nanakzy@gmail.com>
2024-07-30 21:25:35 +0000

    2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
+"""basic flask application"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config:
+    """configuration class application"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
+#initiate application obj with babel
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
@@ -21,6 +23,7 @@ def get_locale():
 
 @app.route('/')
 def index():
+    """renders a basic html template"""
     return render_template('2-index.html')
 
 
