c4bf5e1c1f10f21b976cfad3b150cde11fe6216f
Nanakzy <nanakzy@gmail.com>
2024-07-30 21:48:03 +0000

    5-app.py

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python3
+"""basc flask application"""
+from typing import (
+    Dict, Union
+)
+
 from flask import Flask, g, request, render_template
 from flask_babel import Babel
 
+
+class Config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Instantiate the application object
 app = Flask(__name__)
+app.config.from_object(Config)
+# Wrap the application with Babel
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 users = {
@@ -14,22 +43,32 @@ users = {
 }
 
 
-def get_user():
-    user_id = request.args.get('login_as')
-    if user_id:
-        return users.get(int(user_id))
-    return None
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+    """
+    Validate user login details
+    Args:
+        id (str): user id
+    Returns:
+        (Dict): user dictionary if id is valid else None
+    """
+    return users.get(int(id), 0)
 
 
 @app.before_request
 def before_request():
-    g.user = get_user()
+    """
+    Adds valid user to the global session object `g`
+    """
+    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
 
 
-@app.route('/')
-def index():
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
     return render_template('5-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
